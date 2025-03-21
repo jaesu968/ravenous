a1a61081bccea00adf32e6ee60440f2dd4dfc0e1
jaesu124 <mazterk968@gmail.com>
2025-03-21 11:22:03 -0500

    Business List Component added

---
+++ src/BusinessList.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+
+import Business from '../Business/Business';
+
+// this BusinessList component should use the Business component multiple times 
+// to generate a list of businesses. 
+function BusinessList(props) {
+  return (
+    <div className="BusinessList">
+      {props.businesses.map(business => {
+        return <Business key={business.id} business={business} />;
+      })}
+    </div>
+  );
+}
