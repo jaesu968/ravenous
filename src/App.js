195f2026e3cf951dd6e463ff23ef1e7f53d77061
jaesu124 <mazterk968@gmail.com>
2025-03-24 11:21:55 -0500

    React App Update 6 CSS improved

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import BusinessList from './BusinessList';
 import SearchBar from './SearchBar';
@@ -33,18 +32,6 @@ function App(){
         <h1>ravenous</h1>
         <SearchBar />
         <BusinessList businesses={businesses} />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Codecademy!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
